01f819c3294a2992ada04f7e08063f78bc8ad094
Nikola Spasojevic <nikola.t.spasojevic@gmail.com>
2015-04-08 13:52:05 +0100

    Optical Flow

---
+++ helperfunctions.cpp
@@ -0,0 +1,12 @@
+#include "helperfunctions.h"
+
+HelperFunctions::HelperFunctions()
+{
+
+}
+
+HelperFunctions::~HelperFunctions()
+{
+
+}
+
---
+++ helperfunctions.h
@@ -0,0 +1,12 @@
+#ifndef HELPERFUNCTIONS_H
+#define HELPERFUNCTIONS_H
+
+
+class HelperFunctions
+{
+public:
+    HelperFunctions();
+    ~HelperFunctions();
+};
+
+#endif // HELPERFUNCTIONS_H
